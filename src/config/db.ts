d7456919ddca2bde38f2b6d44a19de9037709714
himanshu dhoundiyal <105155811+Geralt10@users.noreply.github.com>
2026-07-06 16:11:50 +0530

    initial setup

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { env } from "./config.js";
+
+export const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(env.MONGO_URI);
+
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed", error);
+    process.exit(1);
+  }
+};
