4d9bffcf5dd7e23ffb3aae3c507df6dda0f6d294
himanshu dhoundiyal <105155811+Geralt10@users.noreply.github.com>
2026-07-07 12:08:58 +0530

    feature(auth): add foundation

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../utils/ApiError.js";
+
+export const errorMiddleware = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof ApiError) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+};
---
+++ src/modules/auth/auth.validation.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(3, "Name must be at least 3 characters")
+    .max(50, "Name cannot exceed 50 characters")
+    .regex(
+      /^[A-Za-z]+(?:[ '-][A-Za-z]+)*$/,
+      "Please enter a valid name"
+    ),
+
+  email: z
+    .email("Invalid email address")
+    .trim()
+    .toLowerCase(),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "Password must contain at least one special character"
+    ),
+});
